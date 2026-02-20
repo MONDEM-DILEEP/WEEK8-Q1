cd95eba9c354b45bea1716685f637eed4186a008
MONDEM-DILEEP <24331a05i4@mvgrce.edu.in>
2026-02-20 14:15:54 +0530

    Add files via upload

---
+++ Q1-I4.py
@@ -0,0 +1,9 @@
+tuple1 = (23, 1, 25, 30, 10)
+time = tuple1.count(7)
+print(f"Tuple1 is : {tuple1}")
+print(f"The number of times 7 occured in tuple1 is : {time}") 
+
+tuple2 = ('apple', 'banana', 'cherry', 'banana')
+print(f"Tuple2 is : {tuple2}")
+pos = tuple2.index('banana')
+print(f"The index of banana in tuple2 is : {pos}") 
